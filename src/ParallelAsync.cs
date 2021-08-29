7ec464a6bb1aa0fb3763143962cb61b63106078d
Dramiel Leayal <Leayal@users.noreply.github.com>
2021-08-29 22:08:49 +0700

    Add .NET Standard 2.1

--- src/ParallelAsync.cs
+++ src/ParallelAsync.cs
@@ -41,39 +41,9 @@ namespace System.Threading.Tasks
             if (body == null)
                 throw new ArgumentNullException(nameof(body));
 
-            long loopCount = 0;
-            var canceltoken = parallelOptions.CancellationToken;
             using (var bag = CreateCollectionFromSource(source))
             {
-                int parallelAllowance = Math.Min(parallelOptions.MaxDegreeOfParallelism, bag.Count);
-
-                Task[] waiting = new Task[parallelAllowance];
-
-                for (int i = 0; i < parallelAllowance; i++)
-                {
-                    waiting[i] = Task.Factory.StartNew(async () =>
-                    {
-                        foreach (var item in bag.GetConsumingEnumerable())
-                        {
-                            if (!canceltoken.IsCancellationRequested)
-                            {
-                                Interlocked.Increment(ref loopCount);
-                                await body.Invoke(item).ConfigureAwait(false);
-                            }
-                        }
-                    }, canceltoken, TaskCreationOptions.LongRunning, TaskScheduler.Current ?? TaskScheduler.Default).Unwrap();
-                }
-
-                await Task.WhenAll(waiting);
-            }
-
-            if (canceltoken.IsCancellationRequested)
-            {
-                return new ParallelAsyncLoopResult(false, loopCount);
-            }
-            else
-            {
-                return new ParallelAsyncLoopResult(true);
+                return await InnerForEachAsyncWithoutState(bag, parallelOptions, body);
             }
         }
 
@@ -87,6 +57,100 @@ namespace System.Threading.Tasks
         /// <returns>A task that represents <seealso cref="ParallelAsyncLoopResult"/>.</returns>
         public static Task<ParallelAsyncLoopResult> ForEach<TSource>(IEnumerable<TSource> source, Func<TSource, ParallelAsyncLoopState, Task> body)
             => ForEach(source, defaultAsyncOptions, body);
+
+#if NETSTANDARD2_1_OR_GREATER
+        /// <summary>Executes an asynchronously foreach operation on an <seealso cref="IAsyncEnumerable{TSource}"/> in which iterations may run in parallel.</summary>
+        /// <typeparam name="TSource">The type of the data in the source.</typeparam>
+        /// <param name="source">An enumerable data source.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        /// <exception cref="ArgumentNullException">The source argument is null.-or-The body argument is null.</exception>
+        /// <returns>
+        /// This method will return  task that represents <seealso cref="ParallelAsyncLoopResult"/> which will be completed when the async iteration from <paramref name="source"/> and the all executions are both completed or when <seealso cref="ParallelAsyncLoopState.Break"/> or <seealso cref="ParallelAsyncLoopState.Stop"/> is called.
+        /// </returns>
+        public static Task ForEachAsync<TSource>(IAsyncEnumerable<TSource> source, Func<TSource, Task> body)
+            => ForEachAsync(source, defaultAsyncOptions, body);
+
+        /// <summary>Executes an asynchronously foreach operation on an <seealso cref="IAsyncEnumerable{TSource}"/> in which iterations may run in parallel.</summary>
+        /// <typeparam name="TSource">The type of the data in the source.</typeparam>
+        /// <param name="source">An enumerable data source.</param>
+        /// <param name="parallelOptions">An object that configures the behavior of this operation.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        /// <exception cref="ArgumentNullException">The source argument is null.-or-The body argument is null.</exception>
+        /// <returns>
+        /// This method will return  task that represents <seealso cref="ParallelAsyncLoopResult"/> which will be completed when the async iteration from <paramref name="source"/> and the all executions are both completed or when <seealso cref="ParallelAsyncLoopState.Break"/> or <seealso cref="ParallelAsyncLoopState.Stop"/> is called.
+        /// </returns>
+        public static async Task ForEachAsync<TSource>(IAsyncEnumerable<TSource> source, ParallelOptions parallelOptions, Func<TSource, Task> body)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (parallelOptions == null)
+                throw new ArgumentNullException(nameof(parallelOptions));
+            if (body == null)
+                throw new ArgumentNullException(nameof(body));
+
+            using (var bag = new BlockingCollection<TSource>())
+            {
+                var loopstate = new ParallelAsyncLoopState();
+                var t = InnerForEachAsyncWithoutState(bag, parallelOptions, body);
+                await foreach (var item in source)
+                {
+                    if (loopstate.ShouldExitCurrentIteration || loopstate.IsStopped)
+                    {
+                        break;
+                    }
+                    bag.Add(item);
+                }
+                bag.CompleteAdding();
+                await t;
+            }
+        }
+
+        /// <summary>Executes an asynchronously foreach operation on an <seealso cref="IAsyncEnumerable{TSource}"/> in which iterations may run in parallel, and the state of the loop can be monitored and manipulated.</summary>
+        /// <typeparam name="TSource">The type of the data in the source.</typeparam>
+        /// <param name="source">An enumerable data source.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        /// <exception cref="ArgumentNullException">The source argument is null.-or-The body argument is null.</exception>
+        /// <returns>
+        /// This method will return  task that represents <seealso cref="ParallelAsyncLoopResult"/> which will be completed when the async iteration from <paramref name="source"/> and the all executions are both completed or when <seealso cref="ParallelAsyncLoopState.Break"/> or <seealso cref="ParallelAsyncLoopState.Stop"/> is called.
+        /// </returns>
+        public static Task ForEachAsync<TSource>(IAsyncEnumerable<TSource> source, Func<TSource, ParallelAsyncLoopState, Task> body)
+            => ForEachAsync(source, defaultAsyncOptions, body);
+
+        /// <summary>Executes an asynchronously foreach operation on an <seealso cref="IAsyncEnumerable{TSource}"/> in which iterations may run in parallel, and the state of the loop can be monitored and manipulated.</summary>
+        /// <typeparam name="TSource">The type of the data in the source.</typeparam>
+        /// <param name="source">An enumerable data source.</param>
+        /// <param name="parallelOptions">An object that configures the behavior of this operation.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        /// <exception cref="ArgumentNullException">The source argument is null.-or-The body argument is null.</exception>
+        /// <returns>
+        /// This method will return  task that represents <seealso cref="ParallelAsyncLoopResult"/> which will be completed when the async iteration from <paramref name="source"/> and the all executions are both completed or when <seealso cref="ParallelAsyncLoopState.Break"/> or <seealso cref="ParallelAsyncLoopState.Stop"/> is called.
+        /// </returns>
+        public static async Task ForEachAsync<TSource>(IAsyncEnumerable<TSource> source, ParallelOptions parallelOptions, Func<TSource, ParallelAsyncLoopState, Task> body)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (parallelOptions == null)
+                throw new ArgumentNullException(nameof(parallelOptions));
+            if (body == null)
+                throw new ArgumentNullException(nameof(body));
+
+            using (var bag = new BlockingCollection<TSource>())
+            {
+                var loopstate = new ParallelAsyncLoopState();
+                var t = InnerForEachAsync(bag, parallelOptions, body, loopstate);
+                await foreach (var item in source)
+                {
+                    if (loopstate.ShouldExitCurrentIteration || loopstate.IsStopped)
+                    {
+                        break;
+                    }
+                    bag.Add(item);
+                }
+                bag.CompleteAdding();
+                await t;
+            }
+        }
+#endif
 
         /// <summary>
         /// Executes an asynchronously foreach operation on an <seealso cref="System.Collections.IEnumerable"/> in which iterations may run in parallel, and the state of the loop can be monitored and manipulated.
@@ -109,37 +173,80 @@ namespace System.Threading.Tasks
             var loopState = new ParallelAsyncLoopState();
             using (var bag = CreateCollectionFromSource(source))
             {
-                int parallelAllowance = Math.Min(parallelOptions.MaxDegreeOfParallelism, bag.Count);
+                return await InnerForEachAsync(bag, parallelOptions, body, loopState);
+            }
+        }
+
+        private static async Task<ParallelAsyncLoopResult> InnerForEachAsyncWithoutState<TSource>(BlockingCollection<TSource> real_time_source, ParallelOptions parallelOptions, Func<TSource, Task> body)
+        {
+            long loopCount = 0;
+            var canceltoken = parallelOptions.CancellationToken;
+            {
+                int parallelAllowance = Math.Min(parallelOptions.MaxDegreeOfParallelism, Environment.ProcessorCount);
 
                 Task[] waiting = new Task[parallelAllowance];
-                var canceltoken = parallelOptions.CancellationToken;
 
                 for (int i = 0; i < parallelAllowance; i++)
                 {
                     waiting[i] = Task.Factory.StartNew(async () =>
                     {
-                        foreach (var item in bag.GetConsumingEnumerable())
+                        foreach (var item in real_time_source.GetConsumingEnumerable())
                         {
-                            if (canceltoken.IsCancellationRequested)
+                            if (!canceltoken.IsCancellationRequested)
                             {
-                                loopState.Stop();
-                                break;
-                            }
-                            if (loopState.ShouldExitCurrentIteration || loopState.IsStopped)
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                loopState.IncreaseIterationCount();
-                                await body.Invoke(item, loopState).ConfigureAwait(false);
+                                Interlocked.Increment(ref loopCount);
+                                await body.Invoke(item).ConfigureAwait(false);
                             }
                         }
                     }, canceltoken, TaskCreationOptions.LongRunning, TaskScheduler.Current ?? TaskScheduler.Default).Unwrap();
                 }
 
                 await Task.WhenAll(waiting);
+
+                if (canceltoken.IsCancellationRequested)
+                {
+                    return new ParallelAsyncLoopResult(false, loopCount);
+                }
+                else
+                {
+                    return new ParallelAsyncLoopResult(true);
+                }
             }
+        }
+
+        private static async Task<ParallelAsyncLoopResult> InnerForEachAsync<TSource>(BlockingCollection<TSource> real_time_source, ParallelOptions parallelOptions, Func<TSource, ParallelAsyncLoopState, Task> body, ParallelAsyncLoopState loopState)
+        {
+            int parallelAllowance = Math.Min(parallelOptions.MaxDegreeOfParallelism, Environment.ProcessorCount);
+
+            Task[] waiting = new Task[parallelAllowance];
+            var canceltoken = parallelOptions.CancellationToken;
+
+            for (int i = 0; i < parallelAllowance; i++)
+            {
+                waiting[i] = Task.Factory.StartNew(async () =>
+                {
+                    var localvar_loopstate = loopState;
+                    foreach (var item in real_time_source.GetConsumingEnumerable())
+                    {
+                        if (canceltoken.IsCancellationRequested)
+                        {
+                            loopState.Stop();
+                            break;
+                        }
+                        if (localvar_loopstate.ShouldExitCurrentIteration || localvar_loopstate.IsStopped)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            loopState.IncreaseIterationCount();
+                            await body.Invoke(item, localvar_loopstate).ConfigureAwait(false);
+                        }
+                    }
+                }, canceltoken, TaskCreationOptions.LongRunning, TaskScheduler.Current ?? TaskScheduler.Default).Unwrap();
+            }
+
+            await Task.WhenAll(waiting);
 
             if (loopState.ShouldExitCurrentIteration || loopState.IsStopped)
             {
--- src/ParallelAsyncLoopResult.cs
+++ src/ParallelAsyncLoopResult.cs
@@ -3,6 +3,17 @@
     /// <summary>Provides completion status on the execution of a <seealso cref="ParallelAsync"/> loop.</summary>
     public readonly struct ParallelAsyncLoopResult
     {
+#if NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// Gets whether the loop ran to completion, such that all iterations of the loop were executed and the loop didn't receive a request to end prematurely.
+        /// </summary>
+        public readonly bool IsCompleted { get; }
+
+        /// <summary>
+        /// Gets the index of the lowest iteration from which <seealso cref="ParallelAsyncLoopState.Break"/> was called.
+        /// </summary>
+        public readonly long? LowestBreakIteration { get; }
+#else
         /// <summary>
         /// Gets whether the loop ran to completion, such that all iterations of the loop were executed and the loop didn't receive a request to end prematurely.
         /// </summary>
@@ -12,6 +23,8 @@
         /// Gets the index of the lowest iteration from which <seealso cref="ParallelAsyncLoopState.Break"/> was called.
         /// </summary>
         public long? LowestBreakIteration { get; }
+#endif
+
 
         internal ParallelAsyncLoopResult(bool completed) : this(completed, null) { }
 
